34959c873455796d7f680615cc0da91eed3de55a
FRC-Team-333 <39463953+FRC-Team-333@users.noreply.github.com>
2023-02-03 18:43:21 -0500

    LimeLight code

--- PracticeBot2023/src/main/java/frc/robot/subsystems/LimeLight.java
+++ PracticeBot2023/src/main/java/frc/robot/subsystems/LimeLight.java
@@ -33,6 +33,24 @@ public class LimeLight extends SubsystemBase {
 
   @Override
   public void periodic() {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry ty = table.getEntry("ty");
+    double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+
+    // how many degrees back is your limelight rotated from perfectly vertical?
+    double limelightMountAngleDegrees = 25.0;
+
+    // distance from the center of the Limelight lens to the floor
+    double limelightLensHeightInches = 20.0;
+
+    // distance from the target to the floor
+    double goalHeightInches = 60.0;
+
+    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+    //calculate distance
+    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)/Math.tan(angleToGoalRadians);
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
     SmartDashboard.putNumber("LimelightArea", area);
