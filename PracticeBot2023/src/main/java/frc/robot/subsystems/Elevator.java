d031e77c24d3da39bb2220d67ec6f2c306a00bee
Ghozti <62896836+Ghozti@users.noreply.github.com>
2023-02-15 14:46:49 -0500

    - updated pid controller

--- PracticeBot2023/src/main/java/frc/robot/subsystems/Elevator.java
+++ PracticeBot2023/src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,8 +7,6 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
