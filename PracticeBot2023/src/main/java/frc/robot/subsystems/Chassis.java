ae986ffb75a4434c2f0c56105a88c7785e2e07a0
FRC-Team-333 <39463953+FRC-Team-333@users.noreply.github.com>
2023-03-04 14:03:33 -0500

    Refactor Chassis to container classes

--- PracticeBot2023/src/main/java/frc/robot/subsystems/Chassis.java
+++ PracticeBot2023/src/main/java/frc/robot/subsystems/Chassis.java
@@ -21,75 +21,72 @@ import frc.robot.RobotContainer;
 import frc.robot.RobotStates;
 import frc.robot.RobotStates.ChassisStates;
 import frc.robot.RobotStates.ElevatorState;
+import frc.robot.utils.Containers.*;
 
 public class Chassis extends SubsystemBase {
-  /** Creates a new Chassis. */
-  CANSparkMax rightmotor1, rightmotor2, rightmotor3;
-  CANSparkMax leftmotor1, leftmotor2, leftmotor3;
+  Multi_CANSparkMax m_leftSparks, m_rightSparks;
+  Multi_RelativeEncoder m_leftEncoders, m_rightEncoders;
+  MotorControllerGroup m_leftMotors, m_rightMotors;
 
-  RelativeEncoder rightLeaderEncoder, leftLeaderEncoder;
+  Joystick m_stick;
 
-  MotorControllerGroup rightleader;
-  MotorControllerGroup leftLeader;
+  DifferentialDrive m_drive;
 
-  Joystick stick;
+  RobotStates.ChassisStates m_chassisState;
+  PneumaticHub m_hub;
+  DoubleSolenoid m_solenoid;
 
-  DifferentialDrive drive;
-
-  RobotStates.ChassisStates chassisState;
-  PneumaticHub hub;
-  DoubleSolenoid solenoid;
-
-  ElevatorState elevatorState;
+  ElevatorState m_elevatorState;
 
   public Chassis(PneumaticHub hub) {
-    rightmotor1 = new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_R_LEADER, MotorType.kBrushless);
-    rightmotor2 = new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_R_FOLLOWER1, MotorType.kBrushless);
-    rightmotor3 = new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_R_FOLLOWER2, MotorType.kBrushless);
+    m_leftSparks = new Multi_CANSparkMax(
+        new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_L_LEADER, MotorType.kBrushless),
+        new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_L_FOLLOWER1, MotorType.kBrushless),
+        new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_L_FOLLOWER2, MotorType.kBrushless)
+    );
+    m_rightSparks = new Multi_CANSparkMax(
+        new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_R_LEADER, MotorType.kBrushless),
+        new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_R_FOLLOWER1, MotorType.kBrushless),
+        new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_R_FOLLOWER2, MotorType.kBrushless)
+    );
 
-    rightLeaderEncoder = rightmotor1.getEncoder();
+    m_leftEncoders = m_leftSparks.getMultiEncoder();
+    m_rightEncoders = m_rightSparks.getMultiEncoder();
 
-    rightleader = new MotorControllerGroup(rightmotor1, rightmotor2, rightmotor3);
+    m_leftMotors = m_leftSparks.getMotorControllerGroup();
+    m_rightMotors = m_rightSparks.getMotorControllerGroup();
 
-    leftmotor1 = new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_L_LEADER, MotorType.kBrushless);
-    leftmotor2 = new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_L_FOLLOWER1, MotorType.kBrushless);
-    leftmotor3 = new CANSparkMax(Constants.RobotMap.DRIVE_TRAIN_L_FOLLOWER2, MotorType.kBrushless);
-
-    leftLeaderEncoder = leftmotor1.getEncoder();
-
-    leftLeader = new MotorControllerGroup(leftmotor1, leftmotor2, leftmotor3);
-
-    stick = new Joystick(0);
-    drive = new DifferentialDrive(leftLeader, rightleader);
-    this.hub = hub;
-    solenoid = hub.makeDoubleSolenoid(0, 1);
+    m_stick = new Joystick(0);
+    m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+    this.m_hub = hub;
+    m_solenoid = hub.makeDoubleSolenoid(0, 1);
   }
 
-  public void setBreak(){
-    rightmotor1.setIdleMode(IdleMode.kBrake);
-    rightmotor2.setIdleMode(IdleMode.kBrake);
-    rightmotor3.setIdleMode(IdleMode.kBrake);
-    leftmotor1.setIdleMode(IdleMode.kBrake);
-    leftmotor2.setIdleMode(IdleMode.kBrake);
-    leftmotor3.setIdleMode(IdleMode.kBrake);
+  public void setBreak()
+  {
+    setIdleMode(IdleMode.kBrake);
   }
 
-  public void setCoast(){
-    rightmotor1.setIdleMode(IdleMode.kCoast);
-    rightmotor2.setIdleMode(IdleMode.kCoast);
-    rightmotor3.setIdleMode(IdleMode.kCoast);
-    leftmotor1.setIdleMode(IdleMode.kCoast);
-    leftmotor2.setIdleMode(IdleMode.kCoast);
-    leftmotor3.setIdleMode(IdleMode.kCoast);
+  public void setCoast()
+  {
+    setIdleMode(IdleMode.kCoast);
+  }
+
+  private void setIdleMode(IdleMode idleMode)
+  {
+    m_leftSparks.setIdleMode(idleMode);
+    m_rightSparks.setIdleMode(idleMode);
   }
 
   public double getEncodersAverage(){
-    return (rightLeaderEncoder.getPosition() + (-leftLeaderEncoder.getPosition()))/2;
+    return (-m_leftEncoders.getAveragePosition() +
+            m_rightEncoders.getAveragePosition()
+           ) / 2;
   }
 
   public void resetEncoders(){
-    rightLeaderEncoder.setPosition(0);
-    leftLeaderEncoder.setPosition(0);
+    m_leftEncoders.setPosition(0);
+    m_rightEncoders.setPosition(0);
   }
 
   public double getChassisMetersMoved(){
@@ -98,30 +95,30 @@ public class Chassis extends SubsystemBase {
 
   public void arcadeDrive(double x, double y)
   {
-    drive.arcadeDrive(x, y);
+    m_drive.arcadeDrive(x, y);
   }
 
   public void teleopPeriodic(ElevatorState state)
   {
-    double x = stick.getX(), y = -stick.getY();
+    double x = m_stick.getX(), y = -m_stick.getY();
 
     if(RobotContainer.TWO_DRIVER_MODE){
-      if(stick.getTrigger()){//slows down the chassis for lining up
+      if(m_stick.getTrigger()){//slows down the chassis for lining up
         x /= 1.7;
         y /= 2.5;
         setBreak();
       }else{
         setCoast();
       }
-      if(stick.getRawButton(2)){
-        solenoid.set(Value.kForward);
+      if(m_stick.getRawButton(2)){
+        m_solenoid.set(Value.kForward);
         setBreak();
       }else{
-        solenoid.set(Value.kReverse);
+        m_solenoid.set(Value.kReverse);
         setCoast();
       }
     }else{
-      if(stick.getRawButton(7)){
+      if(m_stick.getRawButton(7)){
         x /= 1.7;
         y /= 2.5;
       }
@@ -136,10 +133,10 @@ public class Chassis extends SubsystemBase {
   // any Global Variables related to State.
   @Override
   public void periodic() {
-    chassisState = evaluateState();
-    SmartDashboard.putString("chassis State", chassisState+"");
-    SmartDashboard.putNumber("left chassis encoder", leftLeaderEncoder.getPosition());
-    SmartDashboard.putNumber("right chassis encoder", rightLeaderEncoder.getPosition());
+    m_chassisState = evaluateState();
+    SmartDashboard.putString("chassis State", m_chassisState+"");
+    SmartDashboard.putNumber("left chassis encoder", m_leftEncoders.getAveragePosition());
+    SmartDashboard.putNumber("right chassis encoder", m_rightEncoders.getAveragePosition());
     SmartDashboard.putNumber("Meters moved", getChassisMetersMoved());
     SmartDashboard.putNumber("average chassis encoders", getEncodersAverage());
   }
@@ -147,7 +144,7 @@ public class Chassis extends SubsystemBase {
   public ChassisStates evaluateState()
   {
     // TODO: Should we do this evaluation based on actual Drivetrain Encoders, rather than indirectly by the joystick inputs?
-    double x = stick.getX(), y = stick.getY();
+    double x = m_stick.getX(), y = m_stick.getY();
     if(y > 0.05){
       return ChassisStates.MOVING_FORWARD;
     }
--- PracticeBot2023/src/main/java/frc/robot/utils/Containers.java
+++ PracticeBot2023/src/main/java/frc/robot/utils/Containers.java
@@ -6,15 +6,16 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 
-public class Containers {
-    
-}
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
-class Multi_CANSparkMax
+public class Containers {
+
+public static class Multi_CANSparkMax
 {
     private ArrayList<CANSparkMax> m_sparkMaxArray;
     public Multi_CANSparkMax(CANSparkMax ... sparkMaxArray)
     {
+        m_sparkMaxArray = new ArrayList<>();
         for (CANSparkMax sparkMax : sparkMaxArray) {
             m_sparkMaxArray.add(sparkMax);
         }
@@ -26,20 +27,62 @@ class Multi_CANSparkMax
         {
             sparkMax.setIdleMode(idleMode);
         }
-    } 
+    }
+
+    public Multi_RelativeEncoder getMultiEncoder()
+    {
+        ArrayList<RelativeEncoder> encoderArray = new ArrayList<>();
+        for (CANSparkMax sparkMax : m_sparkMaxArray)
+        {
+            encoderArray.add(sparkMax.getEncoder());
+        }
+        return new Multi_RelativeEncoder(encoderArray);
+    }
+
+    public MotorControllerGroup getMotorControllerGroup()
+    {
+        return new MotorControllerGroup(m_sparkMaxArray.toArray(new CANSparkMax[m_sparkMaxArray.size()]));
+    }
 }
 
-class Multi_RelativeEncoder
+public static class Multi_RelativeEncoder
 {
     private ArrayList<RelativeEncoder> m_encoderArray;
     public Multi_RelativeEncoder(RelativeEncoder ... encoderArray)
     {
+        m_encoderArray = new ArrayList<>();
         for (RelativeEncoder encoder : encoderArray)
         {
             m_encoderArray.add(encoder);
         }
     }
+
+    public Multi_RelativeEncoder(ArrayList<RelativeEncoder> encoderArray)
+    {
+        m_encoderArray = encoderArray;
+    }
+
+    public void setPosition(double pos)
+    {
+        for (RelativeEncoder encoder : m_encoderArray)
+        {
+            encoder.setPosition(pos);
+        }
+    }
+
+    public double getAveragePosition()
+    {
+        double numerator = 0.0;
+        double denominator = 0;
+        for (RelativeEncoder encoder : m_encoderArray)
+        {
+            numerator += encoder.getPosition();
+            denominator += 1;
+        }
+
+        return numerator/denominator;
+    }
 }
 
-
+}
 
