06b083a54e0a0fceb6569cdfab13359d95332aef
Ghozti <62896836+Ghozti@users.noreply.github.com>
2023-03-10 15:40:28 -0500

    - added led code

--- PracticeBot2023/src/main/java/frc/robot/subsystems/ColorSensor.java
+++ PracticeBot2023/src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -12,75 +12,19 @@ import edu.wpi.first.wpilibj.Joystick;
 
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.LEDStrip.FancyLED;
 
 
 public class ColorSensor {
   ColorSensorV3 cs;
-  AddressableLED LED;
-  AddressableLEDBuffer ledBuffer;
   Joystick joy;
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
-
-  private int m_rainbowFirstPixelHue;
+  LEDStrip strip = new LEDStrip(6, 120);
 
   public ColorSensor(){
     cs = new ColorSensorV3(i2cPort);
-    LED = new AddressableLED(6);
     joy = new Joystick(1);
-    ledBuffer = new AddressableLEDBuffer(2000);
-    LED.setLength(ledBuffer.getLength());
-
-    LED.setData(ledBuffer);
-    LED.start();
   }
-
-  public boolean leds(){
-    for (var i = 0; i < ledBuffer.getLength(); i++) {
-      // Sets the specified LED to the RGB values for red
-      ledBuffer.setRGB(i, 200, 0, 122);
-   }
-   
-    LED.setData(ledBuffer);
-    return false;
-  }
-
-  public void blink(int r, int g, int b, int run, int run2){
-    if(run2 <= 50){
-    for(var i = 0; i < ledBuffer.getLength(); i++){
-      ledBuffer.setRGB(i, (run <= 5 ? r : 0) , (run <= 5 ? g : 0), 0);
-    }
-    }else{
-      for(var i = 0; i < ledBuffer.getLength(); i++){
-        ledBuffer.setRGB(i, r,g,b);
-      }
-    }
-    System.out.println(run);
-  }
-
-  private void rainbow() {
-    // For every pixel
-    for (var i = 0; i < ledBuffer.getLength(); i++) {
-      // Calculate the hue - hue is easier for rainbows because the color
-      // shape is a circle so only one value needs to precess
-      final var hue = (m_rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
-      // Set the value
-      ledBuffer.setHSV(i, hue, 255, 128);
-    }
-    // Increase by to make the rainbow "move"
-    m_rainbowFirstPixelHue += 3;
-    // Check bounds
-    m_rainbowFirstPixelHue %= 180;
-  }
-  
- /*public boolean isCone(){
-    boolean isCone = false;
-    if(cs.getBlue() == 4444 && cs.getRed() == 7777 || cs.getColor().toString()=="#5F900F"){
-      isCone = true;
-    } else{
-      isCone = false;
-    }
-    return isCone;
-  }*/
    
   public boolean isCube(){
     boolean isCube = false;
@@ -106,35 +50,22 @@ public class ColorSensor {
   
  
 
-  int run1, run2;
   public void periodic(){
     SmartDashboard.putBoolean("Is Cone?", isCone());
     SmartDashboard.putBoolean("Is Cube?", isCube());
     SmartDashboard.putString("Color", cs.getColor().toString());
     SmartDashboard.putNumber("Blue", cs.getBlue());
     SmartDashboard.putNumber("Red", cs.getRed());
-
-    
-    // Set the data
-   
-    //rainbow();
+    strip.periodic();
 
     if (isCone()) {
-      blink(255, 43, 0, run1, run2);
-      run1++;
-      run2++;
-      if(run1 == 10){
-        run1 = 0;
-      }
-    } else {
-      run2 = 0;
-      for (var i = 0; i < ledBuffer.getLength(); i++) {
-        
-          ledBuffer.setRGB(i, 0, 0, 255);
-        
-        
-      }
+      strip.blink(255, 69, 0);
+    }else if(isCube()){
+      strip.blink(106, 13, 173);
+    }else{
+      strip.resetDurationRun();
+      strip.resetBlinkRun();
+      strip.setFancyDualLayer(FancyLED.ONE_ON_ONE, 0, 0, 100, 100, 0, 0);
     }
-    LED.setData(ledBuffer);
   }
 }
---
+++ PracticeBot2023/src/main/java/frc/robot/subsystems/LEDStrip.java
@@ -0,0 +1,125 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDStrip extends SubsystemBase {
+  /** Creates a new LEDStrip. */
+  AddressableLED LED;
+  AddressableLEDBuffer ledBuffer;
+  int blinkRun, durationRun;
+  int blinkRange, blinkMax, durationMax;
+
+  public LEDStrip(int port, int buffer) {
+    LED = new AddressableLED(port);//default is 6
+    ledBuffer = new AddressableLEDBuffer(buffer);//default is 120
+    LED.setLength(ledBuffer.getLength());
+
+    LED.setData(ledBuffer);
+    LED.start();
+
+    blinkRun = 0;
+    durationRun = 0;
+
+    //default vals for blinking
+    blinkRange = 5;
+    blinkMax = 10;
+    durationMax = 50;
+  }
+
+  public LEDStrip(int port, int buffer, int blinkRange, int blinkMax, int durationMax) {
+    LED = new AddressableLED(port);//default is 6
+    ledBuffer = new AddressableLEDBuffer(buffer);//default is 120
+    LED.setLength(ledBuffer.getLength());
+
+    LED.setData(ledBuffer);
+    LED.start();
+
+    blinkRun = 0;
+    durationRun = 0;
+
+    //default vals for blinking
+    this.blinkRange = blinkRange;
+    this.blinkMax = blinkMax;
+    this.durationMax = durationMax;
+  }
+
+  public void blink(int r, int g, int b){
+    if(durationRun <= durationMax){
+      for(var i = 0; i < ledBuffer.getLength(); i++){
+        ledBuffer.setRGB(i, (blinkRun <= blinkRange ? r : 0) , (blinkRun <= blinkRange ? g : 0), 0);
+      }
+    }else{
+      for(var i = 0; i < ledBuffer.getLength(); i++){
+        ledBuffer.setRGB(i, r,g,b);
+      }
+    }
+  }
+
+  public void set(int r, int g, int b){
+    for(var i = 0; i < ledBuffer.getLength(); i++){
+      ledBuffer.setRGB(i, r, g, b);
+    }
+  }
+
+  public void setFancy(FancyLED ledMode, int r, int g, int b){
+    if(ledMode == FancyLED.ONE_ON_ONE){
+      for(var i = 0; i < ledBuffer.getLength(); i++){
+        if(i % 2 == 0){
+          ledBuffer.setRGB(i, r, g, b);
+        }
+      }
+    }else if(ledMode == FancyLED.KNIGHT_RIDER){
+
+    }else if(ledMode == FancyLED.PULSE){
+
+    }
+  }
+
+  public void setFancyDualLayer(FancyLED ledMode, int r, int g, int b, int r2, int g2, int b2){
+    if(ledMode == FancyLED.ONE_ON_ONE){
+      for(var i = 0; i < ledBuffer.getLength(); i++){
+        if(i % 2 == 0){
+          ledBuffer.setRGB(i, r, g, b);
+        }else{
+          ledBuffer.setRGB(i, r2, g2, b2);
+        }
+      }
+    }else if(ledMode == FancyLED.KNIGHT_RIDER){
+
+    }else if(ledMode == FancyLED.PULSE){
+
+    }
+  }
+
+  public void resetBlinkRun(){
+    blinkRun = 0;
+  }
+
+  public void resetDurationRun(){
+    durationRun = 0;
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    blinkRun++;
+    durationRun++;
+    if(blinkRun == blinkMax){
+      blinkRun = 0;
+    }
+    LED.setData(ledBuffer);
+  }
+
+  public static enum FancyLED{
+      ONE_ON_ONE,
+      KNIGHT_RIDER,
+      PULSE;
+  }
+}
